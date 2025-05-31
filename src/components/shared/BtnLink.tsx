34a0e02690af9a17994f528a25ea86e03939fedc
Micheal-Repo <michealfullstack@gmail.com>
2025-05-31 18:11:47 +0100

    updated

--- src/components/shared/BtnLink.tsx
+++ src/components/shared/BtnLink.tsx
@@ -4,12 +4,14 @@ interface BtnLinkProps {
   className?: string;
 }
 
-export const BtnLink = ({ href, text, className = "" }: BtnLinkProps) => {
+export const BtnLink = ({
+  href, text, className = ""
+}: BtnLinkProps) => {
   return (
     <a
       href={href}
-      className={`px-6 py-3 rounded-full outline-none relative overflow-hidden border dark:bg-violet-600 cursor-pointer ${className}`}
-    >
+      className={`px-6 py-3 rounded-full outline-none relative overflow-hidden border bg-primary cursor-pointer ${className}`}
+      >
       <span className="relative z-10 text-white"> {text}</span>
     </a>
   );
